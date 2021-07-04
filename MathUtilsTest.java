95b7a4d7f06d2dbb62a298ccdb3d8ea38b625a71
Lucas César <lucascesar_35@hotmail.com>
2021-07-04 20:06:03 -0300

    assertArrayEquals

--- MathUtilsTest.java
+++ MathUtilsTest.java
@@ -1,6 +1,10 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +12,21 @@ class MathUtilsTest {
 
 	@Test
 	void test() {
-		System.out.println("Teste sendo executado");
+		MathUtils obj = new MathUtils();
+		int expected = 4;
+		int actual = obj.add(2, 3);
+		assertEquals(expected, actual, "O método add precisa retornar a soma de dois inteiros");
+		
 	}
-
+	
+	@Test
+	void testArray() {
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(0);
+		list.add(2);
+		list.add(5);
+		Integer [] a = {2,0,5};
+		Object [] b = list.toArray();
+		assertArrayEquals(a,b);
+	}
 }
