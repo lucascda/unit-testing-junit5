77725a6100c2f10d1d560d5e64c70b95791c791e
Lucas César <lucascesar_35@hotmail.com>
2021-07-04 19:34:25 -0300

    Setup maven & @Test annotation

---
+++ MathUtilsTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("Teste sendo executado");
+	}
+
+}
